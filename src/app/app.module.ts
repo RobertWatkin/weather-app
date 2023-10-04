c2b20554cd0db0584ca3b2041f1011cdb94f66e3
Robert Watkin <robert.watkin12@gmail.com>
2023-10-04 14:49:46 +0100

    centered the search bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';  // Import FormsModule here
 
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';  // Import the SearchComponent
